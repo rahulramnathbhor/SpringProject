2eb7928b2803aaa52cd61950c696fa60946cc4ba
rahulramnathbhor <rahulb59200@gmail.com>
2024-10-22 10:33:40 +0530

    first commit

---
+++ src/main/java/com/jpa/test/Bootdevtool1Application.java
@@ -0,0 +1,13 @@
+package com.jpa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootdevtool1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootdevtool1Application.class, args);
+	}
+
+}
